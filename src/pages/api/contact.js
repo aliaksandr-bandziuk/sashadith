f0407c4c39c51794afb1ec9a30838600e4e0f249
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-12-28 21:59:36 +0100

    back code

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const { name, phone, email, subject, message } = req.body;
+      const { name, email, message } = req.body;
 
       // Создаем транспорт для отправки почты (замените данными вашего почтового сервера)
       const transporter = nodemailer.createTransport({
@@ -12,17 +12,17 @@ export default async function handler(req, res) {
         port: 465, // Порт для SSL
         secure: true, // Использовать SSL
         auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASSWORD,
+          user: 'sashadith@googlemail.com', // Ваш адрес электронной почты Gmail
+          pass: 'wkgixxjktisvdxcw', // Ваш пароль приложения Gmail
         },
       });
 
       // Опции для отправки письма
       const mailOptions = {
         from: email,
-        to: process.env.EMAIL_USER,
-        subject: `New message from ${name} - ${subject}`,
-        text: `${message}\n\nContact Details:\nName: ${name}\nPhone: ${phone}\nEmail: ${email}`,
+        to: 'sashadith@googlemail.com',
+        subject: `New message from website Sasha Dith`,
+        text: `${message}\n\nContact Details:\nName: ${name}\nEmail: ${email}`,
       };
 
       // Отправляем письмо
